31ab194d44974df442af2eb0bfe41697aabe0c0e
Sergej-Vlasov <sergej.s.vlasov@gmail.com>
2019-04-08 07:33:39 +0100

    add navigation buttons

--- src/components/Navbar/NavItem.tsx
+++ src/components/Navbar/NavItem.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import Button from '@material-ui/core/Button'
+import { Link } from 'react-router-dom'
 
-const navItem: React.FC = () => (
-    <div>nav item</div>
+interface LinkProps {
+    to: string;
+    label: string;
+}
+
+const navItem: React.FC<LinkProps> = ({ to, label }) => (
+    <Link to={to}>
+      <Button variant="raised" color="primary">
+        <span>{label}</span>
+      </Button>
+    </Link>
 );
 
 export default navItem;
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,7 +2,13 @@ import React from 'react';
 import NavItem from './NavItem';
 
 const navbar: React.FC = () => (
-    <div>navbar<NavItem/></div>
+    <div>
+        <NavItem to='/' label='Home'/>
+        <NavItem to='/news' label='News'/>
+        <NavItem to='/login' label='Login'/>
+        <NavItem to='/profile' label='Profile'/>
+        <NavItem to='/404' label='404'/>
+    </div>
 );
 
 export default navbar;
